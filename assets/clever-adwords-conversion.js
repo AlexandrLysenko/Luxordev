4adb861806463ace5382d7f34d1c43bb95950758
Alexandr Lysenko <alex91113011@gmail.com>
2018-04-20 21:56:52 +0300

    initial commit

---
+++ assets/clever-adwords-conversion.js
@@ -0,0 +1,1 @@
+new Image(1,1).src = 'https://www.googleadservices.com/pagead/conversion/987331855/?value='+Shopify.checkout.total_price+'&label=ZmqrCIngnH4Qj_rl1gM&guid=ON&script=0';
---
+++ assets/review-slider.js
@@ -0,0 +1,13 @@
+// jQuery(document).ready(function() {
+// 	$("#review-slider").owlCarousel({
+// 		autoPlay: true, //Set AutoPlay to 3 seconds
+// 		navigation : false,	
+//         pagination : false, 
+// 		items : 1,
+// 		itemsDesktop : [1199,1],
+// 		itemsDesktopSmall : [979,1],
+//         itemsTablet	: [768,1],
+//         autoplayTimeout: 3000,
+           
+// 	});
+// });
